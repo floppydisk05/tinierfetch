b92a7bdf86f5a5fe6657bc1d1a757803c1e73b69
NotAShelf <raf@notashelf.dev>
2023-09-11 01:22:28 +0300

    it fits in a qr code

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 it's tinyfetch but also tinier - you could probably fit this in a QR code
 takes `0.001` to `0.003s` to fetch
 
+edit: it fits in a [QR code](./assets/qr.png)
+
 ## depends
 
 a compiler (I used g++) and `TERM`, `LANG`, `USER`, `SHELL` environmental variables to be set
 
 ## preview
 
-![yeah](./preview.png)
+![yeah](./assets/preview.png)
--- tinierfetch.cpp
+++ tinierfetch.cpp
@@ -1,3 +1,4 @@
 #include <iostream>
 #define e getenv
-using namespace std;int main(){cout<<"\e[31muser:\e[0m "<<e("USER")<<"\n\e[32msh:\e[0m "<<e("SHELL")<<"\n\e[33mterm:\e[0m "<<e("TERM")<<"\n\e[34mlocale:\e[0m "<<e("LANG")<<"\n\e[35mcolors:\e[0m\e[41m  \e[42m  \e[43m  \e[44m  \e[45m  \e[0m\n";}
+#define p <<
+int main() { std::cout p "\e[31muser:\e[0m " p e("USER") p "\n\e[32msh:\e[0m " p e("SHELL") p "\n\e[33mterm:\e[0m " p e("TERM") p "\n\e[34mlocale:\e[0m " p e("LANG") p "\n\x1b[35mcolors: \x1b[0m\x1b[41m \x1b[42m \x1b[43m \x1b[44m " "\x1b[45m \x1b[0m\n"; return 0;}
