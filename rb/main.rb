266c5eb89a6a19b7d5b7c3730d2fa6be723c1e39
Frankie B <git@diskfloppy.me>
2023-09-11 22:00:06 +0100

    Add ruby sauce

---
+++ rb/main.rb
@@ -0,0 +1,5 @@
+puts"\e[31muser:\e[0m #{ENV['USER']}
+\e[32msh:\e[0m #{ENV['SHELL']}
+\e[33mterm:\e[0m #{ENV['TERM']}
+\e[34mlocale:\e[0m #{ENV['LANG']}
+\x1b[35mcolors: \x1b[0m\x1b[41m \x1b[42m \x1b[43m \x1b[44m \x1b[45m \x1b[0m\n"
